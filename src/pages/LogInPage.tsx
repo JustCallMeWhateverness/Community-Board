3a883c0ce26e229d0bdce1f0347f8a4b652866e0
Whateverness <nicolefritz00@gmail.com>
2025-09-20 19:00:13 +0200

    Work in progress login page
    Fixed form for log in page
    Small fix: Removed console.log in CreatePostPage.tsx
    Note: Logging in logic not done

--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -1,7 +1,6 @@
-import { Row, Col } from 'react-bootstrap';
-import Image from '../parts/Image';
+import { Row, Col, Form, Button } from 'react-bootstrap';
 import { useState } from 'react';
-import { createRoot } from 'react-dom/client';
+import { useNavigate } from 'react-router-dom';
 
 
 LogInPage.route = {
@@ -13,20 +12,59 @@ LogInPage.route = {
 export default function LogInPage() {
 
 
+  const [user, setUser] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+  const navigate = useNavigate();
+  function setProperty(event: React.ChangeEvent) {
+    let { name, value } = event.target as HTMLInputElement;
+    setUser({
+      ...user,
+      [name]: value
+    });
+  }
+
+
+  async function sendForm(event: React.FormEvent) {
+
+    event.preventDefault();
+
+    const payload: any = { ...user };
+
+    ;
+    await fetch('/api/users', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(payload)
+    });
+    // navigate to
+    navigate('/');
+  }
   return <>
     <Row>
       <Col>
         <h2 className="text-primary">Log in</h2>
       </Col>
     </Row>
-    <form>
-      <label>Username:
-        <input type="text" />
-      </label>
-      <label>Password:
-        <input type="text" />
-      </label>
-    </form>
+    <Form onSubmit={sendForm}>
+      <Form.Group>
+        <Form.Label className="d-block">
+          <p>Username</p>
+          <Form.Control onChange={setProperty} type="text" name="username" placeholder="Username" />
+        </Form.Label>
+        <Form.Label className="d-block">
+          <p>Email</p>
+          <Form.Control onChange={setProperty} type="text" name="email" placeholder="Email" />
+        </Form.Label>
+        <Form.Label className="d-block">
+          <p>Password</p>
+          <Form.Control onChange={setProperty} type="text" name="password" placeholder="Password" />
+        </Form.Label>
+      </Form.Group>
+      <Button type="submit" className="float-end">Login</Button>
+    </Form>
     <Row>
       <Col md={6}>
         <p>If you don't already have an account, please sign up by clicking the button below</p>
